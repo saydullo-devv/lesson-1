67cedd0b2765fb633d68c01bc2be4d1a509f8124
Saydullo <saydulloxabubillaev@gmail.com>
2024-09-11 20:16:32 +0500

    fixed3

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from  .models import Book
 
-# Register your models here.
+# admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title','author', 'created_at', 'updated_at', 'language','pages',)
--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# model -> jadval yaratish
+class Book(models.Model):
+    title = models.CharField(max_length=200)  # qisqa nom uchun
+    author = models.CharField(max_length=100, default="Unknown Author")  # muallif uchun default qiymat
+    description = models.TextField()  # uzun matin uchun
+    created_at = models.DateTimeField(auto_now_add=True)  # yuklangan sana
+    updated_at = models.DateTimeField(auto_now=True)  # oxirgi o'zgarish
+    ISBN = models.CharField(max_length=13, null=True, unique=True)  # ISBN raqamini matnli maydonda saqlash
+    pages = models.IntegerField(default=0)  # sahifalar
+    language = models.CharField(max_length=30, default="Unknown language")  # til
+    book_category = models.CharField(max_length=50, default="Unknown book_category")  # kitob janiri
+
+    def __str__(self):
+        return self.title
